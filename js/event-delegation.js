8ab7747dd570103d1793a5090637a24d1320c960
phayes02 <123522954+phayes02@users.noreply.github.com>
2023-03-31 16:29:02 -0500

    Add files via upload

---
+++ js/event-delegation.js
@@ -0,0 +1,28 @@
+function getTarget(e) {                          
+  if (!e) {                                      
+    e = window.event;                            
+  }
+  return e.target || e.srcElement;               
+}
+
+function itemDone(e) {                         
+  var target, elParent, elGrandparent;           
+  target = getTarget(e);                         
+
+  if (e.preventDefault) {                        
+    e.preventDefault();                         
+  } else {                                       
+    e.returnValue = false;                      
+  }
+}
+
+var el = document.getElementById('shoppingList');
+if (el.addEventListener) {                      
+  el.addEventListener('click', function(e) {     
+    itemDone(e);                                 
+  }, false);                                     
+} else {                                         
+  el.attachEvent('onclick', function(e) {       
+    itemDone(e);                                
+  });
+}
